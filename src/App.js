c72666f510dc2421f716e8bec5f562152a1e9292
Draktors <enzo.hespel@hotmail.fr>
2019-07-18 15:49:41 +0200

    remove unuse file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ResetGlobalStyle } from "styles/index";
 
 function App() {
   return (
     <div className="App">
+      <ResetGlobalStyle />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
